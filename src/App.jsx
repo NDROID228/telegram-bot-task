364584d7d01740feed62aae6430a77d308e7149c
NDROID228 <djunsonik@gmail.com>
2024-06-10 21:53:15 +0300

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ import "./App.css";
 import { useTelegram } from "./hooks/useTelegram";
 import Header from "./components/Header/Header";
 import { Route, Routes } from "react-router-dom";
-import ProductList from './components/ProductList/ProductList';
-import Form from './components/Form/Form';
+import ProductList from "./components/ProductList/ProductList";
+import Form from "./components/Form/Form";
 
 function App() {
   const { tg } = useTelegram();
@@ -17,8 +17,8 @@ function App() {
     <>
       <Header />
       <Routes>
-        <Route index element={<ProductList /> } />
-        <Route path={'form'} element={<Form />} />
+        <Route index element={<ProductList />} />
+        <Route path={"form"} element={<Form />} />
       </Routes>
     </>
   );
