9537cd7d798600b6acac882e8a1ceedf388b0a49
NDROID228 <djunsonik@gmail.com>
2024-06-10 22:50:38 +0300

    trying to fix x4

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -3,40 +3,40 @@ import "./Form.css";
 import { useTelegram } from "../../hooks/useTelegram";
 
 const Form = () => {
-  const [country, setCountry] = useState("");
-  const [street, setStreet] = useState("");
-  const [subject, setSubject] = useState("physical");
+//   const [country, setCountry] = useState("");
+//   const [street, setStreet] = useState("");
+//   const [subject, setSubject] = useState("physical");
   const { tg } = useTelegram();
 
-  const onSendData = useCallback(() => {
-    const data = {
-      country,
-      street,
-      subject,
-    };
-    tg.sendData(JSON.stringify(data));
-  }, [country, street, subject]);
+//   const onSendData = useCallback(() => {
+//     const data = {
+//       country,
+//       street,
+//       subject,
+//     };
+//     tg.sendData(JSON.stringify(data));
+//   }, [country, street, subject]);
 
-  useEffect(() => {
-    tg.onEvent("mainButtonClicked", onSendData);
-    return () => {
-      tg.offEvent("mainButtonClicked", onSendData);
-    };
-  }, [onSendData]);
+//   useEffect(() => {
+//     tg.onEvent("mainButtonClicked", onSendData);
+//     return () => {
+//       tg.offEvent("mainButtonClicked", onSendData);
+//     };
+//   }, [onSendData]);
 
-  useEffect(() => {
-    tg.MainButton.setParams({
-      text: "Отправить данные",
-    });
-  }, []);
+//   useEffect(() => {
+//     tg.MainButton.setParams({
+//       text: "Отправить данные",
+//     });
+//   }, []);
 
-  useEffect(() => {
-    if (!street || !country) {
-      tg.MainButton.hide();
-    } else {
-      tg.MainButton.show();
-    }
-  }, [country, street]);
+//   useEffect(() => {
+//     if (!street || !country) {
+//       tg.MainButton.hide();
+//     } else {
+//       tg.MainButton.show();
+//     }
+//   }, [country, street]);
 
   const onChangeCountry = (e) => {
     setCountry(e.target.value);
@@ -59,15 +59,15 @@ const Form = () => {
           className={"input"}
           type="text"
           placeholder={"Страна"}
-          value={country}
-          onChange={onChangeCountry}
+        //   value={country}
+        //   onChange={onChangeCountry}
         />
         <input
           className={"input"}
           type="text"
           placeholder={"Улица"}
-          value={street}
-          onChange={onChangeStreet}
+        //   value={street}
+        //   onChange={onChangeStreet}
         />
         <select value={subject} onChange={onChangeSubject} className={"select"}>
           <option value={"physical"}>Физ. лицо</option>
